83319807266376d1cd66d4c59eeed90ccd5d26e1
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-04-24 22:29:55 -0400

    More settings

--- src/publistgenerator/settings/FormatSettings.java
+++ src/publistgenerator/settings/FormatSettings.java
@@ -4,18 +4,29 @@
  */
 package publistgenerator.settings;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
+import publistgenerator.category.OutputCategory;
 
 /**
  *
  * @author Sander
  */
 public class FormatSettings {
+    // General
+    private String format;
+    private File target;
+    // Author info
     private boolean listAllAuthors = false;
+    // Presented
     private boolean underlinePresented = false;
-    private boolean markPresented = false;
-    private List<String> categories;
-    private Map<String, String> categoryNotes;
-    
+    private String presentedText = null;
+    private File presentedImage = null;
+    // Numbering
+    private boolean numberGlobally = false;
+    private boolean numberLocally = false;
+    // Categories
+    private List<OutputCategory> categories;
+    private Map<OutputCategory, String> categoryNotes;
 }
