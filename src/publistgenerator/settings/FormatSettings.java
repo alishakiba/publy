ac5f2ecbf2546f2d209a4a9bfaba2cd4dc0f05ca
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-04-25 14:16:48 -0400

    Getters and setters.

--- src/publistgenerator/settings/FormatSettings.java
+++ src/publistgenerator/settings/FormatSettings.java
@@ -15,7 +15,7 @@ import publistgenerator.category.OutputCategory;
  */
 public class FormatSettings {
     // General
-    private String format;
+    private final String format;
     private File target;
     // Author info
     private boolean listAllAuthors = false;
@@ -29,4 +29,84 @@ public class FormatSettings {
     // Categories
     private List<OutputCategory> categories;
     private Map<OutputCategory, String> categoryNotes;
+
+    public FormatSettings(String format) {
+        this.format = format;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public File getTarget() {
+        return target;
+    }
+
+    public void setTarget(File target) {
+        this.target = target;
+    }
+
+    public boolean isListAllAuthors() {
+        return listAllAuthors;
+    }
+
+    public void setListAllAuthors(boolean listAllAuthors) {
+        this.listAllAuthors = listAllAuthors;
+    }
+
+    public boolean isUnderlinePresented() {
+        return underlinePresented;
+    }
+
+    public void setUnderlinePresented(boolean underlinePresented) {
+        this.underlinePresented = underlinePresented;
+    }
+
+    public String getPresentedText() {
+        return presentedText;
+    }
+
+    public void setPresentedText(String presentedText) {
+        this.presentedText = presentedText;
+    }
+
+    public File getPresentedImage() {
+        return presentedImage;
+    }
+
+    public void setPresentedImage(File presentedImage) {
+        this.presentedImage = presentedImage;
+    }
+
+    public boolean isNumberGlobally() {
+        return numberGlobally;
+    }
+
+    public void setNumberGlobally(boolean numberGlobally) {
+        this.numberGlobally = numberGlobally;
+    }
+
+    public boolean isNumberLocally() {
+        return numberLocally;
+    }
+
+    public void setNumberLocally(boolean numberLocally) {
+        this.numberLocally = numberLocally;
+    }
+
+    public List<OutputCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<OutputCategory> categories) {
+        this.categories = categories;
+    }
+
+    public Map<OutputCategory, String> getCategoryNotes() {
+        return categoryNotes;
+    }
+
+    public void setCategoryNotes(Map<OutputCategory, String> categoryNotes) {
+        this.categoryNotes = categoryNotes;
+    }
 }
