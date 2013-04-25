be8a7cd62e2de39dbfcc965744fb48ad50d6390e
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-04-25 14:18:22 -0400

    Reset to default settings.

--- src/publistgenerator/settings/Settings.java
+++ src/publistgenerator/settings/Settings.java
@@ -12,17 +12,29 @@ import java.util.Map;
  * @author Sander
  */
 public class Settings {
+
     private Map<String, FormatSettings> formatSettings;
-    
+
     public Settings() {
         formatSettings = new HashMap<>();
     }
-    
+
     public FormatSettings getSettings(String format) {
         return formatSettings.get(format);
     }
-    
+
     public void addFormat(String format, FormatSettings settings) {
         formatSettings.put(format, settings);
+    }
+
+    /**
+     * Populates this object with the default values.
+     */
+    public void resetToDefault() {
+        formatSettings.clear();
+        
+        // Default HTML settings
+        HTMLSettings html = new HTMLSettings();
+        formatSettings.put(html.getFormat(), html);
     }
 }
