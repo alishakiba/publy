a5a6bd96216bc3a752a28c74d2d133f7373a4ff3
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-04-27 22:16:30 -0400

    Added more specific settings for when to include abstracts, etc.

--- src/publistgenerator/settings/HTMLSettings.java
+++ src/publistgenerator/settings/HTMLSettings.java
@@ -5,6 +5,7 @@
 package publistgenerator.settings;
 
 import java.io.File;
+import publistgenerator.bibitem.BibItem;
 
 /**
  *
@@ -12,8 +13,56 @@ import java.io.File;
  */
 public class HTMLSettings extends FormatSettings {
 
-    private boolean includeAbstracts;
-    private boolean includeBibtext; // TODO: more specific
+    public enum PublicationType {
+
+        NONE, PUBLISHED, ACCEPTED, ARXIV, ALL;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case NONE:
+                    return "None";
+                case PUBLISHED:
+                    return "Published papers";
+                case ACCEPTED:
+                    return "Accepted manuscripts";
+                case ARXIV:
+                    return "Accepted or on the arXiv";
+                case ALL:
+                    return "All";
+                default:
+                    throw new InternalError("Unrecognized PublicationType: " + this);
+            }
+        }
+
+        public boolean matches(BibItem item) {
+            if (this == ALL) {
+                return true;
+            } else if (this == NONE) {
+                return false;
+            } else {
+                if (item.anyNonEmpty("arxiv")) {
+                    return true;
+                } else if (this == ARXIV) {
+                    return false;
+                } else {
+                    if (item.anyNonEmpty("status")) {
+                        if (this == PUBLISHED) {
+                            return false;
+                        } else {
+                            return item.get("status").startsWith("accepted");
+                        }
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    
+    private PublicationType includeAbstracts = PublicationType.ALL;
+    private PublicationType includeBibtex = PublicationType.ARXIV;
+    private PublicationType includePDF = PublicationType.ARXIV;
     private File header; // TODO: more specific, as I will be generating a lot of javascript for the head. I guess bodyHeader? No, has to include page title and stuff. Perhaps scan for end of head and insert custom js.
     private File footer;
 
@@ -24,6 +73,4 @@ public class HTMLSettings extends FormatSettings {
     public File getFooter() {
         return footer;
     }
-    
-    
 }
