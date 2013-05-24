eb93a8ebbfce1b9a9ba6db864ccb2d927331b7e0
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-05-24 15:40:40 -0400

    Small improvement in exception display.

--- src/publistgenerator/Console.java
+++ src/publistgenerator/Console.java
@@ -16,24 +16,24 @@ import publistgenerator.gui.ConsoleFrame;
  * @author Sander Verdonschot <sander.verdonschot at gmail.com>
  */
 public class Console {
-    
+
     private static final SimpleAttributeSet logAttributes;
     private static final SimpleAttributeSet errorAttributes;
     private static final boolean PRINT_STACKTRACE = true;
     private static JTextPane textPane = null; // A styled text area to log to, if the program was invoked without an attached console
-    
+
     // Static font initialization
     static {
         // Simple black mono-spaced log font
         logAttributes = new SimpleAttributeSet();
         StyleConstants.setFontFamily(logAttributes, "Monospaced");
         StyleConstants.setFontSize(logAttributes, 12);
-        
+
         // The same font, but red for errors
         errorAttributes = new SimpleAttributeSet(logAttributes);
         StyleConstants.setForeground(errorAttributes, Color.red);
     }
-    
+
     public static void log(String format, Object... args) {
         if (textPane == null) {
             if (System.console() == null) {
@@ -43,7 +43,7 @@ public class Console {
                 System.console().flush();
             }
         }
-        
+
         // Re-check, because textPane might have been set by createConsoleFrame()
         if (textPane != null) {
             try {
@@ -54,7 +54,7 @@ public class Console {
             }
         }
     }
-    
+
     public static void error(String format, Object... args) {
         if (textPane == null) {
             if (System.console() == null) {
@@ -64,7 +64,7 @@ public class Console {
                 System.console().flush();
             }
         }
-        
+
         // Re-check, because textPane might have been set by createConsoleFrame()
         if (textPane != null) {
             try {
@@ -75,30 +75,32 @@ public class Console {
             }
         }
     }
-    
+
     public static void except(Throwable exception, String format, Object... args) {
         StringWriter stackTrace = new StringWriter();
         exception.printStackTrace(new PrintWriter(stackTrace));
-        
+
         if (textPane == null) {
             if (System.console() == null) {
                 createConsoleFrame();
             } else {
-                System.console().format(" ERROR: " + format + "%n%s%n", args, exception.toString());
-                
+                System.console().format(" ERROR: " + format + "%n", args);
+                System.console().format("%s%n", exception.toString());
+
                 if (PRINT_STACKTRACE) {
                     System.console().format("%s%n", stackTrace.toString());
                 }
-                
+
                 System.console().flush();
             }
         }
-        
+
         // Re-check, because textPane might have been set by createConsoleFrame()
         if (textPane != null) {
             try {
-                textPane.getDocument().insertString(textPane.getDocument().getLength(), String.format(" " + format + "%n%s%n", args, exception.toString()), errorAttributes);
-                
+                textPane.getDocument().insertString(textPane.getDocument().getLength(), String.format(" " + format + "%n", args), errorAttributes);
+                textPane.getDocument().insertString(textPane.getDocument().getLength(), String.format("%s%n", exception.toString()), errorAttributes);
+
                 if (PRINT_STACKTRACE) {
                     textPane.getDocument().insertString(textPane.getDocument().getLength(), String.format("%s%n", stackTrace.toString()), errorAttributes);
                 }
@@ -108,7 +110,7 @@ public class Console {
             }
         }
     }
-    
+
     public static void setOutputTarget(JTextPane textPane) {
         Console.textPane = textPane;
     }
