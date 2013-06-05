297eb664bcd90f6897f83aaa2022ee374e1a79bf
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-06-05 11:53:04 -0400

    Major cleanup of BibItemWRiter. Removed all unused methods. Also improved title parsing and added a unit test for this.

--- src/publistgenerator/io/BibItemWriter.java
+++ src/publistgenerator/io/BibItemWriter.java
@@ -111,68 +111,29 @@ public abstract class BibItemWriter {
         for (int i = 0; i < names.size(); i++) {
             String name = names.get(i);
 
-            if (i > 0) {
-                if (i == names.size() - 1) { // Last name
-                    if (names.size() > 2) {
-                        result.append(",");
-                    }
+            if (i == 0) {
+                // First author
+                result.append(name);
+            } else if (i < names.size() - 1) {
+                // Middle author
+                result.append(", ");
+                result.append(name);
+            } else {
+                // Last author
+                if (names.size() > 2) {
+                    result.append(",");
+                }
 
-                    if (name.equals("others")) {
-                        result.append(" et~al.");
-                    } else {
-                        result.append(" and ");
-                        result.append(name);
-                    }
+                if (name.equals("others")) {
+                    result.append(" et~al.");
                 } else {
-                    result.append(", ");
+                    result.append(" and ");
                     result.append(name);
                 }
-            } else {
-                result.append(name);
             }
         }
 
         return result.toString();
-    }
-
-    protected String formatNumberSeries(BibItem item) {
-        String volume = item.get("volume");
-
-        if (volume == null || volume.isEmpty()) {
-
-            String number = item.get("number");
-
-            if (number == null || number.isEmpty()) {
-                String series = item.get("series");
-
-                if (series == null || series.isEmpty()) {
-                    return "";
-                } else {
-                    return series;
-                }
-            } else {
-                String result;
-
-                // TODO: fix better
-                /* if (outputState == OutputState.MID_SENTENCE) {
-                 result = "number " + number;
-                 } else {
-                 result = "Number " + number;
-                 }*/
-                result = "number " + number;
-
-                String series = item.get("series");
-
-                if (series == null || series.isEmpty()) {
-                    // bad
-                    return result;
-                } else {
-                    return result + " in " + series;
-                }
-            }
-        } else {
-            return "";
-        }
     }
 
     protected String formatPages(BibItem item) {
@@ -205,26 +166,6 @@ public abstract class BibItemWriter {
             } else {
                 return month + " " + year;
             }
-        }
-    }
-
-    protected String escapeMath(String s) {
-        return s.replaceAll("([^\\\\])\\$", "$1\\\\\\$");
-    }
-
-    protected String outputOld(String string) {
-        if (string == null) {
-            return "";
-        } else {
-            return string;
-        }
-    }
-
-    protected String outputOld(String string, String connective) {
-        if (string == null || string.isEmpty()) {
-            return "";
-        } else {
-            return string + connective;
         }
     }
 
@@ -261,38 +202,30 @@ public abstract class BibItemWriter {
         }
     }
 
-    // TODO: remove? Seems unused
-    protected String addPeriod(String string) {
-        int i = string.length() - 1;
-
-        while (string.charAt(i) == '}') {
-            i--;
-        }
-
-        switch (string.charAt(i)) {
-            case '.':
-            case '!':
-            case '?':
-                return string;
-            default:
-                return string + "."; // TODO: does not work when string ends with braces
-        }
-    }
-
     protected String changeCaseT(String s) {
-        // TODO: handle escaped brackets
-        if (s.contains("{")) {
-            StringBuilder sb = new StringBuilder();
-            int level = 0;
-            boolean first = true;
+        StringBuilder sb = new StringBuilder();
+        int level = 0;
+        boolean first = true;
+        boolean escape = false;
 
-            for (char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) {
+            if (escape) {
+                sb.append(c);
+                escape = false;
+            } else {
                 switch (c) {
                     case '{':
                         level++;
                         break;
                     case '}':
                         level--;
+                        break;
+                    case '\\':
+                        if (level == 0) {
+                            escape = true;
+                        } else {
+                            sb.append(c);
+                        }
                         break;
                     default:
                         if (level == 0) {
@@ -310,10 +243,8 @@ public abstract class BibItemWriter {
                         break;
                 }
             }
-
-            return sb.toString();
-        } else {
-            return Character.toUpperCase(s.charAt(0)) + s.toLowerCase().substring(1);
         }
+
+        return sb.toString();
     }
 }
