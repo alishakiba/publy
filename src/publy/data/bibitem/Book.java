38d14d877eb922dc0a424217886c8120a9527b84
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-07-05 16:56:49 -0400

    Renaming to Publy.

---
+++ src/publy/data/bibitem/Book.java
@@ -0,0 +1,21 @@
+/*
+ */
+package publy.data.bibitem;
+
+/**
+ *
+ * @author Sander Verdonschot <sander.verdonschot at gmail.com>
+ */
+public class Book extends BibItem {
+
+    public Book() {
+        setMandatoryFields("author", "title", "publisher", "year");
+        setOptionalFields("volume", "number", "editor", "series", "address", "edition", "month", "note", "key", "doi", "isbn", "lccn");
+    }
+
+    @Override
+    public String getType() {
+        return "book";
+    }
+    
+}
