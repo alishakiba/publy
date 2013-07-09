af5ec0ea6740526640b78f9c1181626d51eaaa5f
Sander Verdonschot <sander.verdonschot@gmail.com>
2013-07-09 14:59:32 -0400

    Refactor: Moved Author and Venue to the base data package.

---
+++ src/publy/data/Pair.java
@@ -0,0 +1,66 @@
+package publy.data;
+
+/**
+ *
+ * @param <T1> The type of the first argument
+ * @param <T2> The type of the second argument
+ * @author Sander Verdonschot <sander.verdonschot@gmail.com>
+ */
+public class Pair<T1, T2> {
+
+    private T1 first;
+    private T2 second;
+
+    public Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public void setFirst(T1 first) {
+        this.first = first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public void setSecond(T2 second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pair<T1, T2> other = (Pair<T1, T2>) obj;
+        if (this.first != other.first && (this.first == null || !this.first.equals(other.first))) {
+            return false;
+        }
+        if (this.second != other.second && (this.second == null || !this.second.equals(other.second))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + (this.first != null ? this.first.hashCode() : 0);
+        hash = 41 * hash + (this.second != null ? this.second.hashCode() : 0);
+        return hash;
+    }
+}
