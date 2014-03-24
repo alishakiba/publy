335c8f278d5c5d4f9e878703078ada5ea67cdc17
Sander Verdonschot <sander.verdonschot@gmail.com>
2014-03-24 00:03:04 -0400

    Added JavaDoc.

--- src/publy/data/Pair.java
+++ src/publy/data/Pair.java
@@ -16,9 +16,14 @@
 package publy.data;
 
 /**
+ * A generic pair of objects.
+ * <p>
+ * The order of the two objects matters. They can have different types, and even
+ * if they don't, a pair (a, b) is different from a pair (b, a). This is
+ * reflected in the {@code equals} and {@code hashCode} methods.
  *
- * @param <T1> The type of the first argument
- * @param <T2> The type of the second argument
+ * @param <T1> The type of the first object
+ * @param <T2> The type of the second object
  *
  */
 public class Pair<T1, T2> {
@@ -26,23 +31,49 @@ public class Pair<T1, T2> {
     private T1 first;
     private T2 second;
 
+    /**
+     * Creates a new pair from the given objects.
+     *
+     * @param first
+     * @param second
+     */
     public Pair(T1 first, T2 second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Gets the first element of this pair.
+     *
+     * @return the first element
+     */
     public T1 getFirst() {
         return first;
     }
 
+    /**
+     * Sets the first element of this pair.
+     *
+     * @param first the new first element.
+     */
     public void setFirst(T1 first) {
         this.first = first;
     }
 
+    /**
+     * Gets the second element of this pair.
+     *
+     * @return the second element
+     */
     public T2 getSecond() {
         return second;
     }
 
+    /**
+     * Sets the second element of this pair.
+     *
+     * @param second the new second element.
+     */
     public void setSecond(T2 second) {
         this.second = second;
     }
