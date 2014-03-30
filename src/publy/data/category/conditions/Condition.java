86dabe5d91f32659cd664bc11ebaea4c52a7eab4
Sander Verdonschot <sander.verdonschot@gmail.com>
2014-03-30 11:07:04 -0400

    Added JavaDoc.

--- src/publy/data/category/conditions/Condition.java
+++ src/publy/data/category/conditions/Condition.java
@@ -18,38 +18,67 @@ package publy.data.category.conditions;
 import publy.data.bibitem.BibItem;
 
 /**
- *
- *
+ * A boolean condition on publications. A publication either matches this
+ * condition, or it does not. The actual test is implemented in child classes.
  */
 public abstract class Condition {
 
     private boolean inverted;
 
+    /**
+     * Creates a new condition.
+     * <p>
+     * If {@code inverted} is true, any item that would otherwise match this
+     * condition no longer matches it and vice versa.
+     *
+     * @param inverted whether to invert this condition
+     */
     public Condition(boolean inverted) {
         this.inverted = inverted;
     }
 
+    /**
+     * Creates a new condition with the same attributes as the given one.
+     *
+     * @param condition the condition to copy
+     */
     public Condition(Condition condition) {
         this.inverted = condition.inverted;
     }
-    
+
     /**
-     * Checks whether the given item matches this condition.
-     * 
-     * @param item
-     * @return true iff the given item matches this condition.
+     * Checks whether the given publication matches this condition.
+     *
+     * @param item the publication
+     * @return true if the given item matches this condition, false otherwise
      */
     public boolean matches(BibItem item) {
         boolean match = internalMatches(item);
         return (inverted ? !match : match);
     }
-    
+
     protected abstract boolean internalMatches(BibItem item);
 
+    /**
+     * Gets whether this condition is inverted.
+     * <p>
+     * If this is true, any item that would otherwise match this condition no
+     * longer matches it and vice versa.
+     *
+     * @return whether this condition is inverted
+     */
     public boolean isInverted() {
         return inverted;
     }
 
+    /**
+     * Sets whether this condition is inverted.
+     * <p>
+     * Setting this to true makes any item that would otherwise match this
+     * condition no longer match it and vice versa.
+     *
+     * @param inverted the new inverted value
+     */
     public void setInverted(boolean inverted) {
         this.inverted = inverted;
     }
