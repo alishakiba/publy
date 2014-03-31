52205f5c24a18c0d3861a8c50b8a7834b4428b17
Sander Verdonschot <sander.verdonschot@gmail.com>
2014-03-30 20:00:41 -0400

    Added JavaDoc.

--- src/publy/data/category/conditions/TypeCondition.java
+++ src/publy/data/category/conditions/TypeCondition.java
@@ -21,36 +21,86 @@ import java.util.List;
 import publy.data.bibitem.BibItem;
 
 /**
- *
- *
+ * A boolean condition on the type of a publication.
+ * <p>
+ * This can test whether a publication is a journal paper, or if it is of a
+ * well-defined type, i.e. not miscellaneous.
  */
 public class TypeCondition extends Condition {
 
     private List<String> types;
 
+    /**
+     * Creates a new type condition that tests whether the type of a publication
+     * is among the given types.
+     * <p>
+     * If {@code inverted} is true, any item that would otherwise match this
+     * condition no longer matches it and vice versa. In other words, the
+     * condition tests that the type is not among the given types.
+     * <p>
+     * The types are tested against the publication's
+     * {@link BibItem#getOriginalType() original type}.
+     *
+     * @param inverted whether to invert this condition
+     * @param types the types to test for
+     */
     public TypeCondition(boolean inverted, List<String> types) {
         super(inverted);
         this.types = types;
     }
 
+    /**
+     * Creates a new type condition that tests whether the type of a publication
+     * is among the given types.
+     * <p>
+     * If {@code inverted} is true, any item that would otherwise match this
+     * condition no longer matches it and vice versa. In other words, the
+     * condition tests that the type is not among the given types.
+     * <p>
+     * The types are tested against the publication's
+     * {@link BibItem#getOriginalType() original type}.
+     *
+     * @param inverted whether to invert this condition
+     * @param types the types to test for
+     */
     public TypeCondition(boolean inverted, String... types) {
         super(inverted);
         this.types = Arrays.asList(types);
     }
 
+    /**
+     * Creates a new type condition with the same attributes as the given one.
+     *
+     * @param condition the condition to copy
+     */
     public TypeCondition(TypeCondition condition) {
         super(condition);
         types = new ArrayList<>(condition.types);
     }
 
+    /**
+     * Gets the types this condition tests for.
+     *
+     * @return the types
+     */
     public List<String> getTypes() {
         return types;
     }
 
+    /**
+     * Sets the types this condition tests for.
+     * 
+     * @param types the new types
+     */
     public void setTypes(List<String> types) {
         this.types = types;
     }
 
+    /**
+     * Sets the types this condition tests for.
+     * 
+     * @param types the new types
+     */
     public void setTypes(String... types) {
         this.types = Arrays.asList(types);
     }
