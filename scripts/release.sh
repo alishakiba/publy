faa7ab7ff035fcd9ff1887203a9dcecfb7cde53f
Sander Verdonschot <sander.verdonschot@gmail.com>
2015-01-07 17:44:26 -0500

    Improved release script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Take a version number as parameter
+VERSION_FILE="src/publy/gui/UIConstants.java"
+JAR_FILE="dist/Publy.jar"
+PROJECT_DIR=${PWD##*/}
 
+
+# Take a version number as parameter
+: ${1?"Usage: bash $0 NEW_VERSION_NUMBER"}
+version=$1
 
 
 # Verify that this is being run on the release branch
@@ -9,40 +15,63 @@ branch="$(hg branch)"
 
 if [ ! $branch = "release" ]
 then
-  echo "Current branch is \"$branch\". To use this script, switch to the release branch with \"hg update release\", merge all the necessary changes from the main branch, then run this script again."
-  exit 2
-done
+  echo "Current branch is \"$branch\". To use this script, switch to the release branch with \"hg update release\", merge all the necessary changes from the main branch, clean and build in NetBeans, then run this script again."
+  exit 1
+fi
 
 
 # Make sure the version number in UIConstants.java matches the given version number
-codeMajorVersion="$(grep MAJOR_VERSION \"src/publy/gui/UIConstants.java\")"
-codeMinorVersion="$(grep MINOR_VERSION \"src/publy/gui/UIConstants.java\")"
+versionRegex="_VERSION ?= ?([0-9][0-9]*);"
 
-#TODO: check that this is the right line
-#TODO: extract the version number
-#TODO: compare the version numbers
+codeMajorVersionLine="$(grep MAJOR_VERSION $VERSION_FILE)"
+[[ $codeMajorVersionLine =~ $versionRegex ]]
+codeMajorVersion="${BASH_REMATCH[1]}"
+
+codeMinorVersionLine="$(grep MINOR_VERSION $VERSION_FILE)"
+[[ $codeMinorVersionLine =~ $versionRegex ]]
+codeMinorVersion="${BASH_REMATCH[1]}"
+
+codeVersion="${codeMajorVersion}.${codeMinorVersion}"
+
+if [ ! $version = $codeVersion ]
+then
+  echo "ERROR: Version in $VERSION_FILE does not match new version ($codeVersion vs $version)."
+  exit 1
+fi
 
 
+# Make the .jar file executable
+if [ -e "$JAR_FILE" ]
+then
+  echo "ERROR: Jar file $JAR_FILE is missing. Run a clean and build operation in NetBeans to generate this file."
+  exit 1
+fi
 
-
-# Clean and build
-
-
-# Make the .jar file executable (see issue #201)
+chmod +x dist/Publy.jar
 
 
 # Remove the old zip file
+echo -n "Removing old zip file... "
 rm Publy*.zip
+echo "done."
 
 
-# Create a zip file called Publy X.Y.zip, containing:
-#   LICENSE
-#   NOTICE
-#   Some readme file
-#   publications.bib
-#   Publy.jar
-#   data/*
-#   lib/*
+# Create a new zip file
+echo -n "Creating new zip file... "
+ZIP_FILE="Publy $version.zip"
+zip "$ZIP_FILE" "LICENSE" "NOTICE" "publications.bib"
+zip -r "$ZIP_FILE" data lib # Recursively add these directories
+zip -j "$ZIP_FILE" "$JAR_FILE" # Add the jar file without directory information
+echo "done."
+
+
+# Commit these changes
+echo -n "Committing changes... "
+hg commit -m "Updated release zip."
+
 
 # Tag the current revision with the release number
-hg tag #TODO
+hg tag "v$version"
+echo "done."
+
+
