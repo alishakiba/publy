5808f05069c68375fdb8d592edd6b70903905a58
Sander Verdonschot <sander.verdonschot@gmail.com>
2015-09-27 16:57:00 -0400

    Removed unused import.

--- src/publy/algo/PublicationPostProcessor.java
+++ src/publy/algo/PublicationPostProcessor.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumMap;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ListIterator;
