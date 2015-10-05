aeb82a67fa56a42f523774ce9873de4a77f0f38e
Sander Verdonschot <sander.verdonschot@gmail.com>
2015-10-05 17:19:49 -0400

    More work to make TagParser use the Tokenizer too. WARNING: Doesn't compile.

--- src/publy/io/bibtexparser/TagParser.java
+++ src/publy/io/bibtexparser/TagParser.java
@@ -44,91 +44,91 @@ public class TagParser {
 
             switch (type) {
                 case "author":
-                return parseAuthor();
-            case "abbr":
-                return parseAbbreviation();
-            // Ignore valid HTML tags
-            case "a":
-            case "acronym":
-            case "address":
-            case "area":
-            case "b":
-            case "base":
-            case "bdo":
-            case "big":
-            case "blockquote":
-            case "body":
-            case "br":
-            case "button":
-            case "caption":
-            case "cite":
-            case "code":
-            case "col":
-            case "colgroup":
-            case "dd":
-            case "del":
-            case "dfn":
-            case "div":
-            case "dl":
-            case "DOCTYPE":
-            case "dt":
-            case "em":
-            case "fieldset":
-            case "form":
-            case "h1":
-            case "h2":
-            case "h3":
-            case "h4":
-            case "h5":
-            case "h6":
-            case "head":
-            case "html":
-            case "hr":
-            case "i":
-            case "img":
-            case "input":
-            case "ins":
-            case "kbd":
-            case "label":
-            case "legend":
-            case "li":
-            case "link":
-            case "map":
-            case "meta":
-            case "noscript":
-            case "object":
-            case "ol":
-            case "optgroup":
-            case "option":
-            case "p":
-            case "param":
-            case "pre":
-            case "q":
-            case "samp":
-            case "script":
-            case "select":
-            case "small":
-            case "span":
-            case "strong":
-            case "style":
-            case "sub":
-            case "sup":
-            case "table":
-            case "tbody":
-            case "td":
-            case "textarea":
-            case "tfoot":
-            case "th":
-            case "thead":
-            case "title":
-            case "tr":
-            case "tt":
-            case "ul":
-            case "var":
-                Console.warn(Console.WarningType.OTHER, "Ignored HTML tag \"%s\" out of publication context.", type);
-                return null;
-            default:
-                throw new ParseException(String.format("Unrecognized tag \"%s\".", type));
+                    return parseAuthor();
+                case "abbr":
+                    return parseAbbreviation();
+                // Ignore valid HTML tags
+                case "a":
+                case "acronym":
+                case "address":
+                case "area":
+                case "b":
+                case "base":
+                case "bdo":
+                case "big":
+                case "blockquote":
+                case "body":
+                case "br":
+                case "button":
+                case "caption":
+                case "cite":
+                case "code":
+                case "col":
+                case "colgroup":
+                case "dd":
+                case "del":
+                case "dfn":
+                case "div":
+                case "dl":
+                case "DOCTYPE":
+                case "dt":
+                case "em":
+                case "fieldset":
+                case "form":
+                case "h1":
+                case "h2":
+                case "h3":
+                case "h4":
+                case "h5":
+                case "h6":
+                case "head":
+                case "html":
+                case "hr":
+                case "i":
+                case "img":
+                case "input":
+                case "ins":
+                case "kbd":
+                case "label":
+                case "legend":
+                case "li":
+                case "link":
+                case "map":
+                case "meta":
+                case "noscript":
+                case "object":
+                case "ol":
+                case "optgroup":
+                case "option":
+                case "p":
+                case "param":
+                case "pre":
+                case "q":
+                case "samp":
+                case "script":
+                case "select":
+                case "small":
+                case "span":
+                case "strong":
+                case "style":
+                case "sub":
+                case "sup":
+                case "table":
+                case "tbody":
+                case "td":
+                case "textarea":
+                case "tfoot":
+                case "th":
+                case "thead":
+                case "title":
+                case "tr":
+                case "tt":
+                case "ul":
+                case "var":
+                    Console.warn(Console.WarningType.OTHER, "Ignored HTML tag \"%s\" out of publication context.", type);
+                    return null;
+                default:
+                    throw new ParseException(String.format("Unrecognized tag \"%s\".", type));
             }
         } catch (IOException | ParseException ex) {
             in.reset();
@@ -136,55 +136,61 @@ public class TagParser {
         }
     }
 
-    private static Tag parseAuthor() {
-        return null;
-    }
-
-    private static Tag parseAbbreviation() {
-        return null;
-    }
-
-    private static void parseFields(Tag result) {
-        
-    }
-
-    private static Tag parseAuthor(String text) {
+    private static Tag parseAuthor() throws IOException, ParseException {
         Tag result = new Tag(Tag.Type.AUTHOR);
-        
-        parseFields(text, result);
+
+        parseFields(result);
+        tokenizer.match('>');
 
         if (!result.values.containsKey("short")) {
-            Console.error("Author tag is missing mandatory field \"short\":%n%s", text);
+            Console.error("Author tag is missing mandatory field \"short\".");
             return null;
         } else if (!result.values.containsKey("name")) {
-            Console.error("Author tag is missing mandatory field \"name\":%n%s", text);
+            Console.error("Author tag is missing mandatory field \"name\".");
             return null;
         } else {
             return result;
         }
     }
 
-    private static Tag parseAbbreviation(String text) {
+    private static Tag parseAbbreviation() throws IOException, ParseException {
         Tag result = new Tag(Tag.Type.ABBREVIATION);
-        
-        parseFields(text, result);
+
+        parseFields(result);
+        tokenizer.match('>');
 
         if (!result.values.containsKey("short")) {
-            Console.error("Abbreviation tag is missing mandatory field \"short\":%n%s", text);
+            Console.error("Abbreviation tag is missing mandatory field \"short\".");
             return null;
         } else if (!result.values.containsKey("full")) {
-            Console.error("Abbreviation tag is missing mandatory field \"full\":%n%s", text);
+            Console.error("Abbreviation tag is missing mandatory field \"full\".");
             return null;
         } else {
             return result;
         }
     }
-    
-    private static void parseFields(String text, Tag result) {
-        /*Matcher matcher = nameValuePattern.matcher(text);
-        
-        while (matcher.find()) {
-            result.values.put(matcher.group(1), matcher.group(2));
-        }*/
+
+    private static void parseFields(Tag result) throws IOException, ParseException {
+        // name = " value "
+        while (!tokenizer.nextTokenIs('>')) {
+            tokenizer.match(StreamTokenizer.TT_WORD);
+            String name = tokenizer.getLastTokenAsString();
+
+            tokenizer.match('=');
+            tokenizer.setWhiteSpaceMatters(true);
+            tokenizer.match('"');
+
+            StringBuilder value = new StringBuilder();
+
+            while (!tokenizer.nextTokenIs('"')) {
+                tokenizer.match(SPECIAL_CHARACTERS)
+            }
+
+            tokenizer.match(StreamTokenizer.TT_WORD);
+            result.values.put(name, tokenizer.getLastTokenAsString());
+
+            tokenizer.setWhiteSpaceMatters(false);
+            tokenizer.match('"');
+        }
     }
 }
