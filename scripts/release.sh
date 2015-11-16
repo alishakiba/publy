621b8ef5d195d7ac4d9c53203cca0166749b262a
Sander Verdonschot <sander.verdonschot@gmail.com>
2015-11-16 16:57:16 -0500

    Added a final option to release script, and made it run a clean and build. Without the final option, no changes are made to the repository. Resolves #233 and #234.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# Set constants
 VERSION_FILE="src/publy/Constants.java"
 JAR_FILE="dist/Publy.jar"
 PROJECT_DIR=${PWD##*/}
 
 
-# Take a version number as parameter
-: ${1?"Usage: bash $0 NEW_VERSION_NUMBER"}
-version=$1
+# Process parameters
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        --final)
+            opt_final=TRUE
+            shift # past argument
+        ;;
+        *)
+                # unknown option
+        ;;
+    esac
+
+    shift # past argument or value
+done
 
 
 # Verify that this is being run on the release branch
@@ -15,12 +30,12 @@ branch="$(hg branch)"
 
 if [ ! $branch = "release" ]
 then
-  echo "Current branch is \"$branch\". To use this script, switch to the release branch with \"hg update release\", merge all the necessary changes from the main branch, clean and build in NetBeans, then run this script again."
-  exit 1
+    echo "Current branch is \"$branch\". To use this script, switch to the release branch with \"hg update release\", merge all the necessary changes from the main branch, clean and build in NetBeans, then run this script again."
+    exit 1
 fi
 
 
-# Make sure the version number in UIConstants.java matches the given version number
+# Get the version number from Constants.java
 versionRegex="_VERSION ?= ?([0-9][0-9]*);"
 
 codeMajorVersionLine="$(grep MAJOR_VERSION $VERSION_FILE)"
@@ -31,20 +46,21 @@ codeMinorVersionLine="$(grep MINOR_VERSION $VERSION_FILE)"
 [[ $codeMinorVersionLine =~ $versionRegex ]]
 codeMinorVersion="${BASH_REMATCH[1]}"
 
-codeVersion="${codeMajorVersion}.${codeMinorVersion}"
+version="${codeMajorVersion}.${codeMinorVersion}"
 
-if [ ! $version = $codeVersion ]
-then
-  echo "ERROR: Version in $VERSION_FILE does not match new version ($codeVersion vs $version)."
-  exit 1
-fi
+
+# Clean and build the project
+echo -n "Cleaning and building the project... "
+ant clean
+ant jar
+echo "done."
 
 
 # Make the .jar file executable
 if [ ! -e "$JAR_FILE" ]
 then
-  echo "ERROR: Jar file $JAR_FILE is missing. Run a clean and build operation in NetBeans to generate this file."
-  exit 1
+    echo "ERROR: Jar file $JAR_FILE is missing. Run a clean and build operation in NetBeans to generate this file."
+    exit 1
 fi
 
 chmod +x "$JAR_FILE"
@@ -65,13 +81,14 @@ zip -j "$ZIP_FILE" "$JAR_FILE" # Add the jar file without directory information
 echo "done."
 
 
-# Commit these changes
-echo -n "Committing changes... "
-hg commit -A -m "Updated release zip."
+if [ $opt_final ]
+then
+    # Commit these changes
+    echo -n "Committing changes... "
+    hg commit -A -m "Updated release zip."
 
 
-# Tag the current revision with the release number
-hg tag "v$version"
-echo "done."
-
-
+    # Tag the current revision with the release number
+    hg tag "v$version"
+    echo "done."
+fi
